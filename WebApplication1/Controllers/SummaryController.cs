764083de805db37602903604a205ca32158a0232
RotemmBar <roro907@gmail.com>
2023-05-23 16:24:06 +0300

    roomnum

--- WebApplication1/Controllers/SummaryController.cs
+++ WebApplication1/Controllers/SummaryController.cs
@@ -12,34 +12,34 @@ namespace WebApplication1.Controllers
     public class SummaryController : ApiController
     {
 
-        //[HttpGet]
-        //[Route("api/GetSummaryByDate/{PatientId}/{Date}")]
-        //public IHttpActionResult GetSummaryByNum(string PatientId, string date)
-        //{
-        //    try
-        //    {
-        //        SafePlaceDbContextt db = new SafePlaceDbContextt();
-        //        SummaryDto Summary = db.TblSummary.Where(a => a.Summary_Date.ToString().Substring(0, 10) == date && a.WrittenBy == "t" && a.TblWrittenFor.FirstOrDefault().TblTreatment.FirstOrDefault().Patient_Id == PatientId).Select(x => new SummaryDto()
-        //        {
-        //            Summary_Num = x.Summary_Num,
-        //            WrittenBy = x.WrittenBy,
-        //            Summary_Date = x.Summary_Date.ToString().Substring(0, 10),
-        //            ImportanttoNote = x.ImportentToNote,
-        //            Content = x.Content,
-        //            StartTime = (DateTime)x.TblTreatment.FirstOrDefault().StartTime,
-        //            EndTime = (DateTime)x.TblTreatment.FirstOrDefault().EndTime,
-        //            FirstNameP = x.TblTreatment.FirstOrDefault().TblTreats.FirstOrDefault().TblPatient.FirstName,
-        //            LastNameP = x.TblTreatment.FirstOrDefault().TblTreats.FirstOrDefault().TblPatient.LastName
-        //        }).FirstOrDefault();
+        [HttpGet]
+        [Route("api/GetSummaryByDate/{PatientId}/{Date}")]
+        public IHttpActionResult GetSummaryByNum(string PatientId, string date)
+        {
+            try
+            {
+                SafePlaceDbContextt db = new SafePlaceDbContextt();
+                SummaryDto Summary = db.TblSummary.Where(a => a.Summary_Date.ToString().Substring(0, 10) == date && a.WrittenBy == "t" && a.TblWrittenFor.FirstOrDefault().TblTreatment.TblTreats.FirstOrDefault().Patient_Id == PatientId).Select(x => new SummaryDto()
+                {
+                    Summary_Num = x.Summary_Num,
+                    WrittenBy = x.WrittenBy,
+                    Summary_Date = x.Summary_Date.ToString().Substring(0, 10),
+                    ImportanttoNote = x.ImportentToNote,
+                    Content = x.Content,
+                    StartTime = (DateTime)x.TblWrittenFor.FirstOrDefault().TblTreatment.StartTime,
+                    EndTime = (DateTime)x.TblWrittenFor.FirstOrDefault().TblTreatment.EndTime,
+                    FirstNameP = x.TblWrittenFor.FirstOrDefault().TblTreatment.TblTreats.FirstOrDefault().TblPatient.FirstName,
+                    LastNameP = x.TblWrittenFor.FirstOrDefault().TblTreatment.TblTreats.FirstOrDefault().TblPatient.LastName
+                }).FirstOrDefault();
 
 
-        //        return Ok(Summary);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return Content(HttpStatusCode.BadRequest, ex);
-        //    }
-        //}
+                return Ok(Summary);
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
 
         [HttpGet]
         [Route("api/GetAllSummary/{PatientId}")]
@@ -48,12 +48,12 @@ namespace WebApplication1.Controllers
             try
             {
                 SafePlaceDbContextt db = new SafePlaceDbContextt();
-                List<SummaryDto> allSummaries = db.TblTreats.Where(x => x.Patient_Id == PatientId)
+                List<SummaryDto> allSummaries = db.TblSummary.Where(x => x.TblWrittenFor.FirstOrDefault().TblTreatment.TblTreats.FirstOrDefault().Patient_Id == PatientId)
                     .Select(s => new SummaryDto()
                     {
                         Summary_Num = 0,
-                        Summary_Date = s.TblTreatment.Treatment_Date.ToString().Substring(0, 10),
-                        Patient_Id = s.TblPatient.Patient_Id
+                        Summary_Date = s.Summary_Date.ToString().Substring(0, 10),
+                        Patient_Id = s.TblWrittenFor.FirstOrDefault().TblTreatment.TblTreats.FirstOrDefault().Patient_Id
                     }).ToList();
 
                 return Ok(allSummaries);
--- WebApplication1/Controllers/TreatmentController.cs
+++ WebApplication1/Controllers/TreatmentController.cs
@@ -21,136 +21,200 @@ namespace WebApplication1.Controllers
             //DateTime start = daytemp.Date.AddHours(8); //Our earliest appoinment (8:00)
             //DateTime end = daytemp.Date.AddHours(-1); //Our latest appointment (23:00)
             #endregion
-            //var hour = new Dictionary<int, string>();
-            //hour.Add(1, "10:00");
-            //hour.Add(2, "14:00");
-            //hour.Add(3, "16:00");
+            string[] hours = new string[] { "10:00", "14:00", "16:00" }; ///when therapist is available 
 
-            string[] hours = new string[] { "10:00", "14:00", "16:00" }; ///when therapist is avialble 
+            TreatmentDto[] freetreatment = new TreatmentDto[]
+            {
+               new TreatmentDto
+               {
+                   Room_Num=1,
+                   startTimetemp="10:00"
+               },
+                new TreatmentDto
+               {
+                   Room_Num=1,
+                  startTimetemp="14:00"
+               },
+                 new TreatmentDto
+               {
+                   Room_Num=1,
+                   startTimetemp="16:00"
+               },
 
-
-
-            string id = "1";
+            };
+    
+        
+            string therapistid = "1";
             DateTime udate = new DateTime(year, month, day);
 
             SafePlaceDbContextt db = new SafePlaceDbContextt();
 
-            List<TblTreatment> treatsbyday = db.TblTreatment.Where(o => o.Treatment_Date == udate).ToList();
-            List<TblTreatment> treatsbydayandther = treatsbyday.Where(y => y.TblTreats.Any(c => c.Therapist_Id == id)).ToList();
+            List<TblTreatment> treatsbyday = db.TblTreatment.Where(o => o.Treatment_Date == udate).ToList(); //treatment for the day picked
+            List<TblTreatment> treatsbydayandtherapist = treatsbyday.Where(y => y.TblTreats.Any(c => c.Therapist_Id == therapistid)).ToList();
             List<TblTreatment> room1 = treatsbyday.Where(u => u.Room_Num == 1).ToList(); //all treatments happenning TODAY in room 1
             List<TblTreatment> room2 = treatsbyday.Where(u => u.Room_Num == 2).ToList(); //all treatments happenning TODAY in room 2
 
-            var lis = new Dictionary<int, string>();
+            var lis = new Dictionary<int, string>(); 
 
-            foreach (var treatment in treatsbydayandther)
+            foreach (var treatment in treatsbydayandtherapist)
             {
                 DateTime temp = (DateTime)treatment.StartTime;
-                string dem = temp.ToShortTimeString();
-                lis[treatment.Treatment_Id] = dem; //Dictionary ordered by Treatment number- the value is the string of the hour
-            } //Insets hours to dictionary. (Therapist+Day) Based on Treatment Id
-
-            foreach (var tre in lis)
-            {
-                for (int i = 0; i < hours.Length; i++)
-                {
-                    if (tre.Value == hours[i])
-                    {
-                        hours[i] = "Taken";
-                    }
-                }
-            } //Checks if any of the hours are taken based on theapist+day
-            foreach (var r1 in room1)
-            {
-                DateTime t = (DateTime)r1.StartTime;
-                string r1time = t.ToShortTimeString(); //a string of all hours hapenning in room 1 for given day
-
-                for (int i = 0; i < hours.Length; i++)
-                {
-                    if (hours[i] == "Taken")
-                    {
-                        break;
-                    }
-
-                    if (hours[i] == r1time)
-                    {
-                        hours[i] = "Taken";
-                    }
-                }
-
-            } //Checks if any of the hours are taken based on Room1 and day
-            foreach (var r2 in room2)
-            {
-                DateTime t = (DateTime)r2.StartTime;
-                string r2time = t.ToShortTimeString(); //a string of all hours hapenning in room 1 for given day
-
-                for (int i = 0; i < hours.Length; i++)
-                {
-                    if (hours[i] == "Taken")
-                    {
-                        break;
-                    }
-
-                    if (hours[i] == r2time)
-                    {
-                        hours[i] = "Taken";
-                    }
-                }
-
-            }
-
-            #region dic
+                string stringstarttime = temp.ToShortTimeString();
+                lis[treatment.Treatment_Id] = stringstarttime; //Dictionary ordered by Treatment number- the value is the string of the start time
+            } //Inserts hours to dictionary. (Therapist+Day) Based on Treatment Id
+            #region trash
             //foreach (var tre in lis)
             //{
-            //    foreach(var hr in hour)
+            //    for (int i = 0; i < hours.Length; i++)
             //    {
-            //        if (tre.Value == hr.Value)
+            //        if (tre.Value == hours[i])
             //        {
-            //            hour.Remove(hr.Key);
+            //            hours[i] = "Taken";
             //        }
             //    }
             //} //Checks if any of the hours are taken based on theapist+day
-
-
-
-            //foreach (var r1 in room1)
+            ////proceed to check if rooms are available in chosen time.
+            ///
+               //foreach (var r1 in room1)
             //{
             //    DateTime t = (DateTime)r1.StartTime;
             //    string r1time = t.ToShortTimeString(); //a string of all hours hapenning in room 1 for given day
 
-            //    for (int i = 1; i <= hour.Count; i++)
+            //    for (int i = 0; i < hours.Length; i++)
             //    {
-
-            //        if (hour[i] == r1time)
+            //        if (hours[i] == "Taken")
             //        {
-            //            hour.Remove(i);
+            //            continue;
             //        }
+
+            //        if (hours[i] == r1time)
+            //        {
+            //            hours[i] = "Taken";
+            //        }
+
+            //        if (hours[i] != "Taken")
+            //        {
+            //            foreach (var d in freetreatment)
+            //            {
+            //                if(hours[i]==d.startTimetemp)
+            //                {
+            //                    d.Room_Num = 1;
+            //                }
+            //            }
+            //        }            
             //    }
 
             //} //Checks if any of the hours are taken based on Room1 and day
-
 
             //foreach (var r2 in room2)
             //{
             //    DateTime t = (DateTime)r2.StartTime;
             //    string r2time = t.ToShortTimeString(); //a string of all hours hapenning in room 1 for given day
 
-            //    for (int i = 1; i <= hour.Count; i++)
+            //    for (int i = 0; i < hours.Length; i++)
             //    {
-            //        if (hour[i] == r2time)
+            //        if (hours[i] == "Taken")
             //        {
-            //            hour.Remove(i);
+            //            continue;
+            //        }
+
+            //        if (hours[i] == r2time)
+            //        {
+            //            hours[i] = "Taken";
+            //        }
+
+            //        if(hours[i]!="Taken")
+            //        {
+            //            foreach (var d in freetreatment)
+            //            {
+            //                if (hours[i] == d.startTimetemp)
+            //                {
+            //                    d.Room_Num = 2;
+            //                }
+            //            }                  
+
             //        }
             //    }
 
             //}
-            //Checks if any of the hours are taken based on Room2 and day
-            #endregion dic
-            return Ok(hours);
+            //return Ok(freetreatment);
+            #endregion
+
+            foreach (var tre in lis)
+            {
+                foreach (var free in freetreatment)
+                {
+                    if (tre.Value == free.startTimetemp)
+                    {
+                       free.startTimetemp = "Taken";
+                    }
+                }
+            } //Checks if any of the hours are taken based on theapist+day
+              //proceed to check if rooms are available in chosen time.
+
+            foreach (var r1 in room1)
+            {
+                DateTime t = (DateTime)r1.StartTime;
+                string r1time = t.ToShortTimeString(); //a string of all hours hapenning in room 1 for given day
+
+               foreach(var free in freetreatment)
+                {
+                    if (free.startTimetemp == "16:00")
+                    {
+                        free.Room_Num = 2;
+                    }
+                    if (free.startTimetemp == "Taken")
+                    {
+                        continue;
+                    }
+
+                    if (free.startTimetemp == r1time)
+                    {
+                        free.startTimetemp = "Taken";
+                    }
+
+                    //if (free.startTimetemp != "Taken")
+                    //{
+                    //    free.Room_Num = 1;
+                    //}
+                }
+
+            } //Checks if any of the hours are taken based on Room1 and day
+
+            foreach (var r2 in room2)
+            {
+                DateTime t = (DateTime)r2.StartTime;
+                string r2time = t.ToShortTimeString(); //a string of all hours hapenning in room 1 for given day
+
+                foreach (var free in freetreatment)
+                {
+                    if (free.startTimetemp == "Taken")
+                    {
+                        continue;
+                    }
+
+                    if (free.startTimetemp == r2time)
+                    {
+                        free.startTimetemp = "Taken";
+                    }
+
+                    if (free.startTimetemp != "Taken")
+                    {
+                        free.Room_Num = 2;
+                    }
+                    if (free.startTimetemp == "16:00")
+                    {
+                        free.Room_Num = 2;
+                    }
+                }
+
+            } //Checks if any of the hours are taken based on Room1 and day
+            return Ok(freetreatment);       
 
             /////***NEED TO ADD: End times
         }
 
-        
+
         // POST: api/Treatment
         [HttpPost]
         [Route("api/CreateTre")]
@@ -159,21 +223,20 @@ namespace WebApplication1.Controllers
             SafePlaceDbContextt db = new SafePlaceDbContextt();
             int temp = db.TblTreatment.Max(o => o.Treatment_Id) + 1;
 
-            string date = value.TreatmentDate.ToString();
-            string time = value.StartTime.ToString();
+            string date = value.TreatmentDate.ToShortDateString();
+            string time = value.StartTime.ToShortTimeString();
 
             string dateAndTime = date.Trim() + ' ' + time.Trim();
 
-            DateTime dattem = DateTime.Parse(date);
-
+            DateTime dattem = DateTime.Parse(dateAndTime);
             try
             {
                 TblTreatment trea = new TblTreatment();
 
                 trea.Treatment_Id = temp;
                 trea.Treatment_Date = dattem;
-                trea.StartTime = value.StartTime;
-                trea.EndTime = value.StartTime.AddHours(1);
+                trea.StartTime = dattem;
+                trea.EndTime = dattem.AddHours(1);
                 trea.WasDone = value.WasDone;
                 trea.Type_Id = value.Type_Id;
                 trea.Room_Num = value.Room_Num;
@@ -181,13 +244,13 @@ namespace WebApplication1.Controllers
 
                 TblTreats tr = new TblTreats();
 
-                tr.Patient_Id = "1";
+                tr.Patient_Id = "2";
                 tr.Therapist_Id = "1";
                 tr.Treatment_Id = temp;
 
                 db.TblTreats.Add(tr);
                 db.TblTreatment.Add(trea);
-                db.SaveChanges();
+                //db.SaveChanges();
 
                 return Ok();
             }
