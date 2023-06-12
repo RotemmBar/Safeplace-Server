343f5fd27b0c8bbcdc0b100d9187f7d177adf724
RotemmBar <roro907@gmail.com>
2023-06-12 19:20:24 +0300

    new db

--- DATA/TblFile.cs
+++ DATA/TblFile.cs
@@ -22,8 +22,8 @@ namespace DATA
     
         public int File_Num { get; set; }
         public System.DateTime DateSent { get; set; }
-        public string Content { get; set; }
         public int FileType_Num { get; set; }
+        public byte[] Content { get; set; }
     
         public virtual TblFileType TblFileType { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
